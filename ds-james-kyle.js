b51f44803758c8c48c42188c258c9c97e7d5ef08
Bejoy Mathew <bejoygm@gmail.com>
2017-03-21 23:40:49 +0530

    hash map implementation

--- ds-james-kyle.js
+++ ds-james-kyle.js
@@ -58,4 +58,45 @@ class List {
     }
 }
 
+// HashTable
+// var hashTable = new HashTable();
+// hashTable.set('myKey', 'myValue');
+// hasTable.get('myKey'); >> 'myValue'
 
+class HashTable {
+
+    constructor() {
+        this.memory = [];
+    }
+
+    // hashing
+    // hashKey("abc") => 93233
+    // hashKey("xyz") => 123213
+    
+    hashKey(key) {
+        let hash = 0;
+        for (let index = 0; index < key.length; index++) {
+            let code = key.charCodeAt(index);
+            hash = ((hash << 5) - hash) + code | 0;
+        }
+        return hash;
+    }
+
+    get(key) {
+        let address = this.hashkey(key);
+        return this.memory[address];
+    }
+
+    set(key, value) {
+        let address = this.hashKey(key);
+        this.memory[address] = value;
+    }
+
+    remove(key) {
+        let address = this.hashkey(key);
+
+        if (this.memory[address]) {
+            delete this.memory[address];
+        }
+    }
+}
