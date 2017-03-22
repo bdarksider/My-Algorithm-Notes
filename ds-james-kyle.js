e5a60da222312a1755b43e80b7ec84eeed7d7cb6
Bejoy Mathew <bejoygm@gmail.com>
2017-03-22 22:32:17 +0530

    stack completed js

--- ds-james-kyle.js
+++ ds-james-kyle.js
@@ -100,3 +100,28 @@ class HashTable {
         }
     }
 }
+
+class Stack {
+
+    constructor() {
+        this.list = [];
+        this.length = 0;
+    }
+
+    // Push to add items to the top of the stack
+    push(value) {
+        this.length++;
+        this.list.push(value)
+    }
+
+    pop() {
+        if (this.length === 0) return;
+
+        this.length--;
+        return this.list.pop();
+    }
+
+    peek() {
+        return this.list[this.length - 1];
+    }
+}
