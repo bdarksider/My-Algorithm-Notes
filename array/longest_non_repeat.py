8efd919570fc62eeb3251fed9f631396c041f135
Bejoy Mathew <bejoygm@gmail.com>
2018-01-04 17:12:42 +0530

    merge array example: TODO: revisit

--- array/longest_non_repeat.py
+++ array/longest_non_repeat.py
@@ -1,18 +1,27 @@
+# Given a string, find the length of the longest substring
+# without repeating characters.
+
+# Examples:
+
+# Given "abcabcbb", the answer is "abc", which the length is 3.
+
+# Given "bbbbb", the answer is "b", with the length of 1.
+
+# Given "pwwkew", the answer is "wke", with the length of 3.
+# Note that the answer must be a substring,
+# "pwke" is a subsequence and not a substring.
 def longest_non_repeat(s):
-    start, maxlen = 0, 0
-    used_char = {}
+    start, max_length = 0, 0
+    used_chars = {}
     for i, char in enumerate(s):
-        if char in used_char and start <= used_char[char]:
-            start = used_char[char] + 1
+        if char in used_chars and start <= used_chars[char]:
+            start = used_chars[char]+ 1
         else:
-            maxlen = max(maxlen, i-start+1)
-        print(i, used_char, maxlen)
-        used_char[char] = i
-    return maxlen
+            max_length = max(max_length, i-start+1)
+        used_chars[char] = i
+    return max_length
 
-
-
-print(longest_non_repeat("abcabcbb"))
-# print(longest_non_repeat("bbbb"))
-# print(longest_non_repeat("pwwkew"))
-# print(longest_non_repeat("abcabcdefbb"))
+assert longest_non_repeat("abcabcbb") == 3
+assert longest_non_repeat("bbbb") == 1
+assert longest_non_repeat("pwwkew") == 3
+assert longest_non_repeat("abcabcdefbb") == 6
